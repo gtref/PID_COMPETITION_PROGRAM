dd4e9a1b47d4f0ed1703a68cec2165f1b05f8896
G-type <162748908+gtref@users.noreply.github.com>
2026-09-25 21:46:58 +1200

    Add some extra helpers from the origional source.

--- PID_COMPETITION_CODE/src/main.cpp
+++ PID_COMPETITION_CODE/src/main.cpp
@@ -29,7 +29,7 @@ void usercontrol(void);
 // Controller1            controller    primary
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
-int main() {
+int main(void) {
     Brain.Timer.clear();
 
     competition::bStopAllTasksBetweenModes = false;
@@ -52,6 +52,65 @@ int main() {
     return 0;
 }
 
-void vexcodeInit() {
+void vexcodeInit(void) {
+    waitUntil(Brain.Screen.pressing());
+
+    initrs();
+    Brain.Timer.clear();
+    Brain.Screen.print("Device initialization...");
+    Brain.Screen.setCursor(2, 1);
+    wait(1000, msec);
+
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1, 1);
+    Brain.Screen.setFillColor(red);
+    Brain.Screen.drawRectangle(0, 0, 240, 240);
+    Brain.Screen.setFillColor(blue);
+    Brain.Screen.drawRectangle(240, 0, 240, 240);
+    Brain.Screen.setFillColor(transparent);
+    waitUntil(Brain.Screen.pressing());
+
+     if ((Brain.Screen.xPosition() >= 0 && Brain.Screen.xPosition() <= 239) && (Brain.Screen.yPosition() >= 0 && Brain.Screen.yPosition() <= 239)) {
+        red_alliance = true;
+        blue_alliance = false;
+    }
+    else {
+        red_alliance = false;
+        blue_alliance = true;
+    }
+    Brain.Screen.clearScreen();
+    Brain.Screen.setFillColor(transparent);
+    wait(1000, msec);
+
+    if (red_alliance) {
+        Brain.Screen.print("Red Alliance selected...");
+    }
+    else {
+        Brain.Screen.print("Blue Alliance selected...");
+    }
+     wait(1000, msec);
     
+    // Calibrate the drivetrain Inertial before starting
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1, 1);
+    wait(50, msec);
+}
+
+void initrs(void) {
+    int systemTime = Brain.Timer.systemHighResolution();
+    double batteryCurrent = Brain.Battery.current();
+    double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
+
+    // Combine these values into a single integer
+    int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
+
+    // Set the seed
+    srand(seed);
+}
+
+// Helper to make playing sounds from the V5 in VEXcode easier and
+// keeps the code cleaner by making it clear what is happening.
+void playVexcodeSound(const char *soundName) { // Direct copy from og source
+  printf("VEXPlaySound:%s\n", soundName);
+  wait(5, msec);
 }
